333a5d074df92ae155d73e07e153b39fda00a2c5
guosh <guosh@digitalcitylab.com.cn>
2023-01-17 19:02:52 +0800

    feature:init project

---
+++ Event/src/main/java/com/digitalcitylab/yun/olap/event/interfaces/controller/EventController.java
@@ -0,0 +1,10 @@
+package com.digitalcitylab.yun.olap.event.interfaces.controller;
+
+/**
+ * @Description:
+ * @Author: guosh
+ * @Email: guosh@digitalcitylab.com.cn
+ * @Date: 2023/1/17
+ **/
+public class EventController {
+}
